a4c86aa4e8e3752ad2cddebbc489a0d03e3626df
veerabv <bveera0608@gmail.com>
2024-06-27 12:24:24 +0530

    adding link in readme file and send response conditionally

--- app.js
+++ app.js
@@ -3,8 +3,16 @@ const http = require("http");
 const server = http.createServer((req,res) => {
     console.log(req.url , req.method , req.headers);
     // process.exist();  // this line will stop the server (i.e stop the event loop process)
-    
+    const url = req.url;
     // send response
+
+    if(url === '/'){
+        res.write('<html>');
+        res.write('<head><title>Enter message</title></head>');
+        res.write('<body><form action = "/message" method = "POST"><input type="text" name="message" /><button type="submit">save</button></form></body>');
+        res.write('</html>');
+        return res.end();
+    }
     res.setHeader('Content-Type' , 'text/html');
     res.write('<html>');
     res.write('<head><title>Node server</title></head>');
---
+++ readme.md
@@ -0,0 +1,8 @@
+ ## http headers reference link
+
+
+```sh
+https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers
+``` 
+
+
