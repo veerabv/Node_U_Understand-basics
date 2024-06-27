c40916c477b7373d0d870f14e5a8d8c6bcf5a9a2
veerabv <bveera0608@gmail.com>
2024-06-27 11:45:41 +0530

    send response back to server

--- app.js
+++ app.js
@@ -3,6 +3,15 @@ const http = require("http");
 const server = http.createServer((req,res) => {
     console.log(req.url , req.method , req.headers);
     // process.exist();  // this line will stop the server (i.e stop the event loop process)
+    
+    // send response
+    res.setHeader('Content-Type' , 'text/html');
+    res.write('<html>');
+    res.write('<head><title>Node server</title></head>');
+    res.write('<body><h1>Hello form my node js server</h1></body>');
+    res.write('</html>');
+    res.end();
+
 })
 
 server.listen(8080,() => {
