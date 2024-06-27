9f7e90c1a6de0585c575e109e3b7009dba1489f5
veerabv <bveera0608@gmail.com>
2024-06-27 11:37:45 +0530

    log  some of the req object key

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const http = require("http");
 
 const server = http.createServer((req,res) => {
-    console.log(req);
+    console.log(req.url , req.method , req.headers);
+    // process.exist();  // this line will stop the server (i.e stop the event loop process)
 })
 
 server.listen(8080,() => {
     console.log("Server is running on port 8080");
 })
+
